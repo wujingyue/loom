88ee645fbc8fca87dcb706f20988c785c1106c80
Jingyue Wu <wujingyue@gmail.com>
2012-12-13 09:41:48 -0500

    Add LoomEnter(Exit)Process to global_ctors(dtors)

--- lib/Instrumenter/CheckInserter.cpp
+++ lib/Instrumenter/CheckInserter.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/CallSite.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "rcs/IDAssigner.h"
 #include "rcs/IdentifyBackEdges.h"
@@ -23,6 +24,7 @@ struct CheckInserter: public FunctionPass {
   virtual void getAnalysisUsage(AnalysisUsage &AU) const;
   virtual bool doInitialization(Module &M);
   virtual bool runOnFunction(Function &F);
+  virtual bool doFinalization(Module &M);
 
  private:
   static void InsertAfter(Instruction *I, Instruction *Pos);
@@ -31,14 +33,17 @@ struct CheckInserter: public FunctionPass {
   void insertCycleChecks(Function &F);
   void insertBlockingChecks(Function &F);
   void insertThreadChecks(Function &F);
+  bool addCtorOrDtor(Module &M, Function &F, const string &GlobalName);
 
   // scalar types
   Type *VoidType, *IntType;
+  FunctionType *IniterType;
 
   // checks
   Function *CycleCheck;
   Function *BeforeBlocking, *AfterBlocking;
-  Function *EnterThread;
+  Function *EnterThread, *ExitThread;
+  Function *EnterProcess, *ExitProcess;
 };
 }
 
@@ -73,14 +78,14 @@ bool CheckInserter::doInitialization(Module &M) {
   IntType = Type::getInt32Ty(M.getContext());
 
   // setup checks
-  // These checks share the same function type fortunately.
   FunctionType *CheckType = FunctionType::get(VoidType, IntType, false);
-  // setup loom_cycle_check
+  IniterType = FunctionType::get(VoidType, false);
+
   CycleCheck = Function::Create(CheckType,
                                 GlobalValue::ExternalLinkage,
                                 "LoomCycleCheck",
                                 &M);
-  // setup loom_before/after_blocking
+
   BeforeBlocking = Function::Create(CheckType,
                                     GlobalValue::ExternalLinkage,
                                     "LoomBeforeBlocking",
@@ -89,10 +94,23 @@ bool CheckInserter::doInitialization(Module &M) {
                                    GlobalValue::ExternalLinkage,
                                    "LoomAfterBlocking",
                                    &M);
-  // setup loom_enter/exit_thread
+
   EnterThread = Function::Create(CheckType,
                                  GlobalValue::ExternalLinkage,
                                  "LoomEnterThread",
+                                 &M);
+  ExitThread = Function::Create(CheckType,
+                                GlobalValue::ExternalLinkage,
+                                "LoomExitThread",
+                                &M);
+
+  EnterProcess = Function::Create(IniterType,
+                                  GlobalValue::ExternalLinkage,
+                                  "LoomEnterProcess",
+                                  &M);
+  ExitProcess = Function::Create(IniterType,
+                                 GlobalValue::ExternalLinkage,
+                                 "LoomExitProcess",
                                  &M);
 
   // Return true because we added new function declarations.
@@ -109,18 +127,66 @@ void CheckInserter::checkFeatures(Function &F) {
       }
     }
   }
+  // TODO: We do not support the situation where pthread_exit is called via
+  // a function pointer. Need to check this assumption.
 }
 
 bool CheckInserter::runOnFunction(Function &F) {
-  IdentifyThreadFuncs &IDF = getAnalysis<IdentifyThreadFuncs>();
-
   checkFeatures(F);
 
   insertCycleChecks(F);
   insertBlockingChecks(F);
-  if (IDF.isThreadFunction(F)) {
-    insertThreadChecks(F);
+  // Run insertThreadChecks after the other two.
+  insertThreadChecks(F);
+
+  return true;
+}
+
+bool CheckInserter::doFinalization(Module &M) {
+  bool Result = false;
+  Result |= addCtorOrDtor(M, *EnterProcess, "llvm.global_ctors");
+  Result |= addCtorOrDtor(M, *ExitProcess, "llvm.global_dtors");
+  return Result;
+}
+
+bool CheckInserter::addCtorOrDtor(Module &M,
+                                  Function &F,
+                                  const string &GlobalName) {
+  // We couldn't directly add an element to a constant array, because doing so
+  // changes the type of the constant array.
+
+  // element type of llvm.global_ctors/llvm.global_dtors
+  StructType *ST = StructType::get(IntType,
+                                   PointerType::getUnqual(IniterType),
+                                   NULL); // end with null
+
+  // Move all existing elements of <GlobalName> to <Constants>.
+  vector<Constant *> Constants;
+  if (GlobalVariable *GlobalCtors = M.getNamedGlobal(GlobalName)) {
+    ConstantArray *CA = cast<ConstantArray>(GlobalCtors->getInitializer());
+    for (unsigned j = 0; j < CA->getNumOperands(); ++j) {
+      ConstantStruct *CS = cast<ConstantStruct>(CA->getOperand(j));
+      assert(CS->getType() == ST);
+      // Assume nobody is using the highest priority, so that <F> will be the
+      // first (last) to run as a ctor (dtor).
+      assert(!cast<ConstantInt>(CS->getOperand(0))->isMinValue(true));
+      Constants.push_back(CS);
+    }
+    GlobalCtors->eraseFromParent();
   }
+  // Add <F> with the highest priority.
+  Constants.push_back(ConstantStruct::get(ST,
+                                          ConstantInt::get(IntType, INT_MIN),
+                                          &F,
+                                          NULL));
+  // Create the new <GlobalName> (llvm.global_ctors or llvm.global_dtors).
+  ArrayType *ArrType = ArrayType::get(ST, Constants.size());
+  new GlobalVariable(M,
+                     ArrType,
+                     true,
+                     GlobalValue::AppendingLinkage,
+                     ConstantArray::get(ArrType, Constants),
+                     GlobalName);
 
   return true;
 }
@@ -178,10 +244,39 @@ void CheckInserter::insertBlockingChecks(Function &F) {
 }
 
 void CheckInserter::insertThreadChecks(Function &F) {
-  CallInst::Create(EnterThread,
-                   ConstantInt::get(IntType, -1), // invalid id
-                   "",
-                   F.begin()->getFirstInsertionPt());
-  // LoomEnterThread registers LoomExitThread as a cleanup routine. Therefore,
-  // we needn't insert call to LoomExitThread here.
+  IdentifyThreadFuncs &IDF = getAnalysis<IdentifyThreadFuncs>();
+
+  // Add LoomExitThread before each pthread_exit().
+  for (Function::iterator B = F.begin(); B != F.end(); ++B) {
+    for (BasicBlock::iterator I = B->begin(); I != B->end(); ++I) {
+      CallSite CS(I);
+      if (CS) {
+        Function *Callee = CS.getCalledFunction();
+        if (Callee->getName() == "pthread_exit") {
+          CallInst::Create(ExitThread,
+                           ConstantInt::get(IntType, -1), // invalid ID
+                           "",
+                           I);
+        }
+      }
+    }
+  }
+
+  // If <F> is a thread function, add EnterThread at its entry, and add
+  // ExitThread at its exits.
+  if (IDF.isThreadFunction(F)) {
+    CallInst::Create(EnterThread,
+                     ConstantInt::get(IntType, -1), // invalid ID
+                     "",
+                     F.begin()->getFirstInsertionPt());
+    for (Function::iterator B = F.begin(); B != F.end(); ++B) {
+      TerminatorInst *TI = B->getTerminator();
+      if (isa<ReturnInst>(TI)) {
+        CallInst::Create(ExitThread,
+                         ConstantInt::get(IntType, -1), // invalid ID
+                         "",
+                         B->getTerminator());
+      }
+    }
+  }
 }
